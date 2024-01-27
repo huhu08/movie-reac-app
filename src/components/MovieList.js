fbe77d77280f190cf2b7349de66716f349a6e71a
Huda <hudaelzubair@hotmail.com>
2024-01-28 02:33:39 +0300

    Adding items into card

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,7 +7,7 @@ const MovieList =({movies})=>{
         {movies.map((movie,id)=> 
 
         (
-        <div className="d-flex justify-content-start m-3">
+        <div className="card d-flex justify-content-start m-3">
             <img src={movie.posterURL} alt='pic'/>
             <h3>{movie.id}</h3>
             <h3>{movie.title}</h3>
